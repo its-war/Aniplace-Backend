741f4da199483852a735c8daa41df96986d2546c
Karlos Warney <karloswarney@gmail.com>
2022-05-21 17:35:45 -0300

    Bug na verificação do login: corrigido

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: "localhost",
-    user: "Warney",
-    password: "27652765",
+    user: "root",
+    password: "",
     port: 3306,
     database: "otakuland"
 });
--- src/routes/loginRouter.js
+++ src/routes/loginRouter.js
@@ -2,8 +2,10 @@ const loginRouter = require('express').Router();
 
 const efetuarLogin = require('../controllers/login/efetuarLogin');
 const loggout = require('../controllers/login/loggout');
+const verificar = require('../controllers/login/validarLogin')
 
 loginRouter.post('/efetuarLogin', efetuarLogin);
 loginRouter.get('/loggout', loggout);
+loginRouter.get('/validar', verificar.eAutorizado);
 
 module.exports = loginRouter;
