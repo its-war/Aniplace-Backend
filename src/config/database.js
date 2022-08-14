775bf1441e091cdcf6b21f386e2cfa5447739039
Karlos Warney <karloswarney@gmail.com>
2022-08-14 16:31:54 -0300

    ALTER - Arquivo de configuração alterado para reagir ao .env local e ao da Heroku.

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/aniplace').then(() => {
+mongoose.connect(process.env.DATABASEURL).then(() => {
     //console.log("Conectou");
     //mongodb://localhost/aniplace
     //mongodb+srv://itswar:27652765@aniplace.a7rjwww.mongodb.net/?retryWrites=true&w=majority
@@ -11,17 +11,3 @@ mongoose.connect('mongodb://localhost/aniplace').then(() => {
 });
 
 module.exports = mongoose;
-
-/*
-# Modelo de como salvar no mongoDB (campos com 'default' podem ser ignorados aqui)
-
-let user = mongoose.model('usuario');
-new user({
-    nome: "Teste2",
-    email: "teste2@teste.com",
-    username: "teste2",
-    senha: "123",
-    chave: "hh1232",
-    registro: "hoje"
-}).save().then(() => {console.log("Cadastrou");}).catch((err) => {console.log("Erro: " + err)});
- */
