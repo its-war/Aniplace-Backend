2de6e3b36085f16f9d72fa33e12b524aed86b99e
Karlos Warney <85758065+its-war@users.noreply.github.com>
2023-02-22 20:09:06 -0300

    FIX - Remoção de dados sensíveis

--- src/config/database.js
+++ src/config/database.js
@@ -5,8 +5,6 @@ mongoose.Promise = global.Promise;
 mongoose.connect(process.env.DATABASEURL).then(() => {
     //console.log("Conectou");
     //mongodb://localhost/aniplace
-    //mongodb+srv://itswar:27652765@aniplace.a7rjwww.mongodb.net/?retryWrites=true&w=majority
-    //mongoose.connection.db.dropDatabase();
 }).catch((err) => {
     console.log(err);
 });
